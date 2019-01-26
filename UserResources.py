4095326f954a2e494bbe6415b53beb631194339d
Danial <danial.mirza@outlook.com>
2019-01-26 14:27:27 +0000

    added UserLoginResource

--- UserResources.py
+++ UserResources.py
@@ -22,6 +22,28 @@ class UserLoginResource(object):
         else:
             resp.body = json.dumps({'auth': True, 'location': userQuery['location']})
 
+class UserRegisterResource(object):
+
+    def __init__(self, pymongo):
+        self.db = pymongo
+
+    def on_post(self, req, resp):
+        data = req.stream.read(req.content_length or 0)
+        user = json.loads(data)
+
+        username = user['username']
+        password = user['password']
+        location = user['location']
+
+        userQuery = self.db.users.find_one({'username':username})
+
+        if userQuery is not None:
+            resp.body = json.dumps({'message': "Username already exists, please enter another"})
+
+        else:
+            result = self.db.users.insert_one({'username':username, 'password':password, 'location':location})
+
+            resp.body = json.dumps({'message': "Welcome " + str(username) + '!'})
 
 
 
--- app.py
+++ app.py
@@ -3,13 +3,14 @@ import waitress
 from pymongo import MongoClient
 
 from HelloWorldResources import HelloWorldResource
-from UserResources import UserLoginResource
+from UserResources import UserLoginResource, UserRegisterResource
 
 if __name__ == '__main__':
     api = falcon.API()
     pymongo = MongoClient('localhost', 27017)
     api.add_route("/hello", HelloWorldResource(pymongo.draw))
     api.add_route("/user/login", UserLoginResource(pymongo.draw))
+    api.add_route("/user/register", UserRegisterResource(pymongo.draw))
 
     print("Created API")
 
