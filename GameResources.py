95f379afe86f8ca3e924c639ec87029163214995
Harris Mirza <mirza.harris@gmail.com>
2019-01-26 16:11:57 +0000

    Updated Game Resources

--- GameResources.py
+++ GameResources.py
@@ -1,6 +1,7 @@
 import json
 import falcon
 
+
 class CreateGameResource(object):
 
     def __init__(self, pymongo):
@@ -18,10 +19,93 @@ class CreateGameResource(object):
             "host": reqJson["username"],
             "numPlayers": reqJson["numPlayers"],
             "players": [],
-            "initialImages": [[]*reqJson["numPlayers"]],
-            "overlayImages": [[] * reqJson["numPlayers"]],
-            "guesses": [[] * reqJson["numPlayers"]]
+            "initialImages": {},
+            "overlayImages": {},
+            "guesses": {},
         })
+
+        resp.body = json.dumps({"message": "Created Game!"})
+
+
+class GameInfoResource(object):
+    def __init__(self, pymongo):
+        self.db = pymongo
+
+    def on_post(self, req, resp):
+        data = req.stream.read(req.content_length or 0)
+        reqJson = json.loads(data)
+
+        # Find game and return object
+        gameInfo = self.db.games.find_one({"host": reqJson["host"]})
+
+        if gameInfo is None:
+            resp.body = json.dumps({"message": "Game does not exist"})
+        else:
+            resp.body = json.dumps(gameInfo)
+
+
+class RecieveInitialImageResource(object):
+    def __init__(self, pymongo):
+        self.db = pymongo
+
+    def on_post(self, req, resp):
+        data = req.stream.read(req.content_length or 0)
+        reqJson = json.loads(data)
+
+        # Find game and return object
+        gameInfo = self.db.games.find_one({"host": reqJson["host"]})
+
+        if gameInfo is not None:
+            initialImages = gameInfo['initialImages']
+            initialImages[reqJson['username']] = reqJson["image"]
+
+            self.db.games.update_one({"host": reqJson["host"]}, {"$set": {"initialImages", initialImages}})
+            resp.body = json.dumps({"message": "Successfully added image to DB"})
+        else:
+            resp.body = json.dumps({"message": "Game does not exist"})
+
+
+class ReceiveOverlayImagesResource(object):
+    def __init__(self, pymongo):
+        self.db = pymongo
+
+    def on_post(self, req, resp):
+        data = req.stream.read(req.content_length or 0)
+        reqJson = json.loads(data)
+
+        # Find game and return object
+        gameInfo = self.db.games.find_one({"host": reqJson["host"]})
+
+        if gameInfo is not None:
+            overlayImages = gameInfo['overlayImages']
+            overlayImages[reqJson['username']] = reqJson["images"]
+
+            self.db.games.update_one({"host": reqJson["host"]}, {"$set": {"overlayImages", overlayImages}})
+            resp.body = json.dumps({"message": "Successfully added images to DB"})
+        else:
+            resp.body = json.dumps({"message": "Game does not exist"})
+
+
+class ReceiveGuessesResource(object):
+    def __init__(self, pymongo):
+        self.db = pymongo
+
+    def on_post(self, req, resp):
+        data = req.stream.read(req.content_length or 0)
+        reqJson = json.loads(data)
+
+        # Find game and return object
+        gameInfo = self.db.games.find_one({"host": reqJson["host"]})
+
+        if gameInfo is not None:
+            guesses = gameInfo['guesses']
+            guesses[reqJson['username']] = reqJson["guesses"]
+
+            self.db.games.update_one({"host": reqJson["host"]}, {"$set": {"guesses", guesses}})
+            resp.body = json.dumps({"message": "Successfully added guesses to DB"})
+        else:
+            resp.body = json.dumps({"message": "Game does not exist"})
+
 
 class JoinGameResource(object):
 
@@ -46,10 +130,3 @@ class JoinGameResource(object):
             players.append(username)
 
             self.db.games.update_one({'host': host}, {"$set": {'players': players}})
-
-
-
-
-
-
-
